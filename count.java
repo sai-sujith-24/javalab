1516b3d9a5e966d36b9d136e2e7f1c53004df8dd
saisujith-18 <saisujith2406@gmail.com>
2026-08-23 19:21:33 +0530

    arrays and counter

---
+++ count.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class count {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number: ");
+        int n = sc.nextInt();
+
+        int count = 0;
+
+        while (n != 0) {
+            count++;
+            n = n / 10;
+        }
+
+        System.out.println("Digits = " + count);
+    }
+}    
+
