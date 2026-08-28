6687522c0d664bbc8537b55830f0fd5fe90757cb
saisujith-18 <saisujith2406@gmail.com>
2026-08-28 11:31:25 +0530

    lcm

---
+++ lcm.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+class lcm{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter two numbers: ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int x = a;
+        int y = b;
+
+        while (y != 0) {
+            int temp = y;
+            y = x % y;
+            x = temp;
+        }
+
+        int gcd = x;
+        int lcm = (a * b) / gcd;
+
+        System.out.println("LCM = " + lcm);
+    }
+}
