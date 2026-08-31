a48ef28d9f77c94cf86db0cb20d6aa137759bfe3
saisujith-18 <saisujith2406@gmail.com>
2026-08-31 19:01:23 +0530

    non

---
+++ non.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+class non {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter string: ");
+        String str = sc.nextLine();
+
+        for (int i = 0; i < str.length(); i++) {
+            int count = 0;
+
+            for (int j = 0; j < str.length(); j++) {
+                if (str.charAt(i) == str.charAt(j))
+                    count++;
+            }
+
+            if (count == 1) {
+                System.out.println("First non-repeating character = " + str.charAt(i));
+                break;
+            }
+        }
+    }
+}
