7dd606131daa261eda546f4592bcb23ceab25203
saisujith-18 <saisujith2406@gmail.com>
2026-09-01 17:28:38 +0530

    don

---
+++ don.java
@@ -0,0 +1,22 @@
+class don {
+    public static void main(String[] args) {
+        int[] a = {64, 25, 12, 22, 11};
+
+        for (int i = 0; i < a.length - 1; i++) {
+            int min = i;
+
+            for (int j = i + 1; j < a.length; j++) {
+                if (a[j] < a[min])
+                    min = j;
+            }
+
+            int temp = a[i];
+            a[i] = a[min];
+            a[min] = temp;
+        }
+
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + " ");
+        }
+    }
+}
