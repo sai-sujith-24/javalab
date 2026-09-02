2b1957c170013e0a7cdd2762deddce2d98ade796
saisujith-18 <saisujith2406@gmail.com>
2026-09-02 23:06:38 +0530

    upper case

---
+++ upp.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+class upp {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter string: ");
+        String str = sc.nextLine();
+
+        int upper = 0;
+        int lower = 0;
+        int digits = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+
+            if (Character.isUpperCase(ch))
+                upper++;
+            else if (Character.isLowerCase(ch))
+                lower++;
+            else if (Character.isDigit(ch))
+                digits++;
+        }
+
+        System.out.println("Uppercase = " + upper);
+        System.out.println("Lowercase = " + lower);
+        System.out.println("Digits = " + digits);
+    }
+}
