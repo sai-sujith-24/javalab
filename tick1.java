4570c1edd3734866c8b14ec8c714e2e390c4e034
saisujith-18 <saisujith2406@gmail.com>
2026-09-04 12:53:55 +0530

    tick1

---
+++ tick1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class tick1 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for (int i = n; i > 0; i--) {
+            System.out.println("tick " + i);
+        }
+    }
+}
