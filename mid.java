7182ddb4d6a10ddb538905669f74627a8bea50e8
saisujith-18 <saisujith2406@gmail.com>
2026-09-04 17:36:19 +0530

    mid

---
+++ mid.java
@@ -0,0 +1,9 @@
+class mid{
+    public static void main(String[] args) {
+        int[] a = {10, 20, 30, 40, 50};
+
+        int middle = a.length / 2;
+
+        System.out.println("Middle Element = " + a[middle]);
+    }
+}
