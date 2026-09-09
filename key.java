5a26729d2e9046c9ae56e8c9e049b153f6c914b9
saisujith-18 <saisujith2406@gmail.com>
2026-09-09 22:14:53 +0530

    this

---
+++ key.java
@@ -0,0 +1,20 @@
+class StudentThis {
+    String name;
+    int age;
+
+    StudentThis(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    void display() {
+        System.out.println("Name = " + name);
+        System.out.println("Age = " + age);
+    }
+
+    public static void main(String[] args) {
+        StudentThis s = new StudentThis("Ravi", 20);
+
+        s.display();
+    }
+}
