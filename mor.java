6d3abaa796539966f15cea9fc02724662b06a12a
saisujith-18 <saisujith2406@gmail.com>
2026-09-12 22:10:02 +0530

    mor

---
+++ mor.java
@@ -0,0 +1,10 @@
+class mor {
+    public static void main(String[] args) {
+        int n = 27;
+
+        if (n % 3 == 0)
+            System.out.println("Multiple of 3");
+        else
+            System.out.println("Not a Multiple of 3");
+    }
+}
