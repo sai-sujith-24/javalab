3e9b7772ca797d681138432b3dac5783aa97379f
saisujith-18 <saisujith2406@gmail.com>
2026-09-13 13:11:24 +0530

    static

---
+++ hemi.java
@@ -0,0 +1,9 @@
+class hemi {
+    public static void main(String[] args) {
+        int[] a = {10, 20, 30, 40, 50};
+
+        int last = a[a.length - 1];
+
+        System.out.println("Last Element = " + last);
+    }
+}
