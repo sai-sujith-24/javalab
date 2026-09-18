f576efc31c301ae1e069a03e60f774d08d7d77ef
saisujith-18 <saisujith2406@gmail.com>
2026-09-18 21:58:26 +0530

    msg

---
+++ str.java
@@ -0,0 +1,13 @@
+public class str {
+    public static void main(String[] args) {
+        String str1 = "Hello";
+        String str2 = "Hello";
+        String str3 = new String("Hello");
+        System.out.println("Using == operator:");
+        System.out.println("str1 == str2 : " + (str1 == str2));
+        System.out.println("str1 == str3 : " + (str1 == str3));
+        System.out.println("using equals() method:");
+        System.out.println("str1.equals(str2) : " + str1.equals(str2));
+        System.out.println("str1.equals(str3) : " + str1.equals(str3));
+    }
+}
