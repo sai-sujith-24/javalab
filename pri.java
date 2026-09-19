43c8883a8ed86568e07a5466277269ae813ca94a
saisujith-18 <saisujith2406@gmail.com>
2026-09-19 11:26:03 +0530

    pri

---
+++ pri.java
@@ -0,0 +1,7 @@
+class pri {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("7 x " + i + " = " + (7 * i));
+        }
+    }
+}
