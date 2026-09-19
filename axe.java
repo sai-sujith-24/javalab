0daf1b84f7ea098dd085e55dd500c7d207e7c2b5
saisujith-18 <saisujith2406@gmail.com>
2026-09-19 11:23:59 +0530

    axe

---
+++ axe.java
@@ -0,0 +1,10 @@
+class axe {
+    public static void main(String[] args) {
+        int n = 36;
+
+        if (n % 6 == 0)
+            System.out.println("Multiple of 6");
+        else
+            System.out.println("Not a Multiple of 6");
+    }
+}
