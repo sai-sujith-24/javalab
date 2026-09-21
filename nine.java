a57b8272ca704aa26411e2953c818922d8bf0632
saisujith-18 <saisujith2406@gmail.com>
2026-09-21 22:05:31 +0530

    nine

---
+++ nine.java
@@ -0,0 +1,10 @@
+class nine {
+    public static void main(String[] args) {
+        int n = 81;
+
+        if (n % 9 == 0)
+            System.out.println("Divisible by 9");
+        else
+            System.out.println("Not Divisible by 9");
+    }
+}
