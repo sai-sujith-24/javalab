3bc48c844c5fb9c399b454128c0858adb9984f6c
saisujith-18 <saisujith2406@gmail.com>
2026-09-26 11:21:17 +0530

    gin

---
+++ gin.java
@@ -0,0 +1,10 @@
+class gin {
+    public static void main(String[] args) {
+        int n = 24;
+
+        if (n % 2 == 0 && n % 3 == 0)
+            System.out.println("Divisible by both 2 and 3");
+        else
+            System.out.println("Not Divisible by both");
+    }
+}
