459c8f4e2feacb4fc405048f9dcc2256b8801426
saisujith-18 <saisujith2406@gmail.com>
2026-09-26 11:23:03 +0530

    uru

---
+++ uru.java
@@ -0,0 +1,11 @@
+class uru {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = 30;
+
+        double average = (a + b + c) / 3.0;
+
+        System.out.println("Average = " + average);
+    }
+}
